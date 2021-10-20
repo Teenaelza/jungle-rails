05546732b7fffa6b72d484ccd71336f7a163d8d9
Teenaelza <teena.thaliath@gmail.com>
2021-10-20 18:20:30 +0000

    Bug-Fix Feature User Sign UP

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,5 +1,6 @@
 class SessionsController < ApplicationController
   def new
+    
   end
 
   def create
@@ -8,7 +9,7 @@ class SessionsController < ApplicationController
       session[:user_id] = user.id
       redirect_to '/'
     else
-      redirect_to '/login'
+      render :new
     end
   end
   def destroy
